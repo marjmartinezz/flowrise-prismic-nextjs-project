cfd694f8ed0b471d8a6faeb3fbd00b7527ce651e
Marj Martinez <marj@majordigital.com>
2024-10-03 18:56:36 +0100

    feat: Add variations to Hero Slice

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -1,15 +1,13 @@
-import { Content } from '@prismicio/client';
-import { PrismicNextImage } from '@prismicio/next';
-import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from '@prismicio/react';
-import Bounded from '@/app/components/Bounded';
-import Button from '@/app/components/Button';
-import Heading from '@/app/components/Heading';
-
-/**
- * Props for `Hero`.
- */
-export type HeroProps = SliceComponentProps<Content.HeroSlice>;
-
+import { Content } from "@prismicio/client";
+import { PrismicNextImage  } from "@prismicio/next";
+import {
+  JSXMapSerializer,
+  PrismicRichText,
+  SliceComponentProps,
+} from "@prismicio/react";
+import Bounded from "@/app/components/Bounded";
+import Button from "@/app/components/Button";
+import Heading from "@/app/components/Heading";
 
 const components: JSXMapSerializer = {
   heading1: ({ children }) => (
@@ -29,32 +27,71 @@ const components: JSXMapSerializer = {
 };
 
 /**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
-    <Bounded
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      <div className="grid grid-cols-1 place-items-center text-center">
-        <PrismicRichText
-          field={slice.primary.heading}
-          components={components}
-        />
-        <PrismicRichText
-          field={slice.primary.body}
-          components={components    }
-        />
-        <Button field={slice.primary.button_link} className=" mb-8 md:mb-10">
-          {slice.primary.button_text}
-        </Button>
-        <PrismicNextImage
-          field={slice.primary.image}
-          className="drop-shadow-xl max-w-4xl w-full "
-        />
-      </div>
-    </Bounded>
+    <>
+      {slice.variation === "default" && (
+        <Bounded
+          data-slice-type={slice.slice_type}
+          data-slice-variation={slice.variation}
+        >
+          <div className="grid grid-cols-1 place-items-center text-center">
+            <PrismicRichText
+              field={slice.primary.heading}
+              components={components}
+            />
+            <PrismicRichText
+              field={slice.primary.body}
+              components={components}
+            />
+            <Button field={slice.primary.button_link} className="mb-8 md:mb-10">
+              {slice.primary.button_text}
+            </Button>
+            <PrismicNextImage
+              field={slice.primary.image}
+              className="drop-shadow-xl max-w-4xl w-full"
+            />
+          </div>
+        </Bounded>
+      )}
+
+      {slice.variation === "horizontal" && (
+        <Bounded
+          data-slice-type={slice.slice_type}
+          data-slice-variation={slice.variation}
+        >
+          <div className="grid grid-cols-1 md:grid-cols-2 place-items-center">
+            <div className="grid grid-rows-[1fr,auto,auto] h-fit">
+              <PrismicRichText
+                field={slice.primary.heading}
+                components={components}
+              />
+              <PrismicRichText
+                field={slice.primary.body}
+                components={components}
+              />
+              <Button
+                field={slice.primary.button_link}
+                className="mb-8 md:mb-10"
+              >
+                {slice.primary.button_text}
+              </Button>
+            </div>
+            <PrismicNextImage
+              field={slice.primary.image}
+              className="drop-shadow-xl max-w-4xl w-full"
+            />
+          </div>
+        </Bounded>
+      )}
+    </>
   );
 };
 
